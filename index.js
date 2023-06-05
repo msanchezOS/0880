3dad0d06d5f0e1d99f37a9d8525254e4d2110997
mssnzz <manuelarturosanz@gmail.com>
2023-06-05 07:20:53 -0700

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors"; // Importa el middleware cors
 
 import routes from "./api/routes/routes.js";
 const app = express();
+
+app.use(cors()); // Utiliza el middleware cors
 
 app.use(express.json({ extended: false }));
 
