d3d6b4a351dd32689f3ab12fdcf08582dc777727
msanchezOS <135194038+msanchezOS@users.noreply.github.com>
2024-06-07 10:17:31 -0400

    Update routes.js

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,7 +3,10 @@ import {
   getCentros,
   getCentro,
   getTiposCentros,
-  getCentrosDistinct
+  getCentrosDistinct,
+  createCentro,
+  updateCentro,
+  deleteCentro
 } from "../controllers/centros.controller.js";
 import {
   getProvincias,
@@ -12,15 +15,25 @@ import {
 import {
   getMedicos,
   getEspecialidades,
-  getMedico
+  getMedico,
+  createMedico,
+  updateMedico,
+  deleteMedico
 } from "../controllers/medicos.controller.js";
 import { actualizar } from "../controllers/actualizardatos.controller.js";
+import { register, login, authenticate } from "../controllers/auth.controller.js";
+
 
 const router = Router();
-
+router.post("/register", register);
+router.post("/login", login);
 
 // GET all Employees
 router.get("/centros", getCentros);
+router.post("/centros", createCentro); // Añadimos la nueva ruta POST
+router.put("/centros/:id", updateCentro); // Añadimos la nueva ruta PUT
+router.delete("/centros/:id", deleteCentro); // Añadir la nueva ruta DELETE
+
 router.get("/centros/:id", getCentro);
 router.get("/tipos/centros/", getTiposCentros);
 router.get("/tipos/centros/distinct", getCentrosDistinct);
@@ -30,6 +43,9 @@ router.get("/provincias/municipios", getMunicipios);
 router.get("/medicos", getMedicos);
 router.get("/medicos/especialidades", getEspecialidades);
 router.get("/medico/:id", getMedico);
+router.post("/medicos", createMedico);
+router.put("/medicos/:id", updateMedico);
+router.delete("/medicos/:id", deleteMedico);
 
 router.post("/actualizar/datos", actualizar);
 
